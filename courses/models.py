054e083ec7ee18d5ba8bd4dcce456e058abc68e7
RRedladeRR <kuznetsovroman775@gmail.com>
2026-03-04 21:04:03 +0200

    Add Content model and related content types for modular course structure

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 
@@ -42,3 +44,36 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name="Contents", on_delete=models.CASCADE, verbose_name="Topic")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, verbose_name="Content type", limit_choices_to={"model__in": ("text", "file", "image", "video", "url")})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
+
+class BaseContent(models.Model):
+    owner = models.ForeignKey(User, related_name="%(class)s_related", on_delete=models.CASCADE, verbose_name="Author")
+    title = models.CharField(max_length=500, verbose_name="Title")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Text(BaseContent):
+    body = models.TextField(verbose_name="Text")
+
+class File(BaseContent):
+    file = models.FileField(upload_to="files", verbose_name="File")
+
+class Image(BaseContent):
+    file = models.FileField(upload_to="images", verbose_name="Image")
+
+class Video(BaseContent):
+    url = models.URLField(verbose_name="Video")
+
+class URL(BaseContent):
+    url = models.URLField(verbose_name="Link")
