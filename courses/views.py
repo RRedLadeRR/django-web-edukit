2143de2b6f25481ea4e04dedaca7855fe09db59c
RRedladeRR <kuznetsovroman775@gmail.com>
2026-03-18 19:13:55 +0200

    Implement course management views: add Create, Update, and Delete views; enhance ManageCourseListView with ownership mixin

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .mixins import OwnerCourseEditMixin, OwnerCourseMixin
 from .models import Course
 
 # Create your views here.
@@ -8,10 +10,15 @@ from .models import Course
 class HomeView(TemplateView):
     template_name = "base.html"
 
-class ManageCourseListView(ListView):
+class ManageCourseListView(OwnerCourseMixin,ListView):
     model = Course
     template_name = "courses/manage/course/list.html"
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs.filter(owner=self.request.user)
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = "courses/manage/course/delete.html"
