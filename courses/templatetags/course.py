ce990b12c7a744a2b4e6cc9c553fa4d99038afe1
RRedladeRR <kuznetsovroman775@gmail.com>
2026-04-01 19:30:48 +0300

    Add model_name filter and update content_list template for better content display

---
+++ courses/templatetags/course.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library
+
+@register.filter
+def model_name(object):
+    try:
+        return object._meta.model_name
+    except AttributeError:
+        return None
