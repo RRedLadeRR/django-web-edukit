57b720be4af96173c8f62c67f2fff681a1098d38
RRedladeRR <kuznetsovroman775@gmail.com>
2026-06-10 19:59:27 +0300

    Add serializers and permissions for course content retrieval

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from courses.models import Subject, Module, Course
+from courses.models import Subject, Module, Course, Content
 
 class ModuleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,3 +16,28 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ["id", "title", "slug"]
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+    
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ["order", "item"]
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ["order", "title", "description", "contents"]
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ["id", "subject", "title", "slug", "overview", "created", "owner", "modules"]
